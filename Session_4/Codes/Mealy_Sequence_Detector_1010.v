382c98a4aafd52736632d66d1fcebd43e064f693
Balaji Srinivasan <93860462+pyCoder03@users.noreply.github.com>
2024-01-14 23:27:56 +0530

    Create Mealy_Sequence_Detector_1010.v

---
+++ Session_4/Codes/Mealy_Sequence_Detector_1010.v
@@ -0,0 +1,39 @@
+//Mealy Non-Overlapping Sequence Detector-1010
+module Sequence_detector_Mealy(
+    D,Rst,Clk,Y,state
+    );
+    input D,Clk,Rst;
+    output reg Y;
+    output reg[1:0] state;
+    reg[1:0] next_state;
+    always @(*) begin
+        
+        // Next State Combinational Logic
+        case(state)
+            2'b00: begin
+                next_state<=(D)?2'b01:2'b00;
+                Y<=1'b0;
+            end
+            2'b01: begin
+                next_state<=(D)?2'b01:2'b10;
+                Y<=1'b0;
+            end
+            2'b10: begin
+                next_state<=(D)?2'b11:2'b00;
+                Y<=1'b0;
+            end
+            default: begin   
+                next_state<=(D)?2'b01:2'b00;
+                Y<=~D;
+            end
+        endcase
+        
+    end 
+    
+    always @(posedge Clk or posedge Rst) begin
+        if(Rst==1'b1)
+            state<=2'b00;
+        else
+            state<=next_state;
+    end
+endmodule
