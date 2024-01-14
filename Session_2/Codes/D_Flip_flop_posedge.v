5950efeccf47a1da9748b2ee241c892c0a701bf1
Balaji Srinivasan <93860462+pyCoder03@users.noreply.github.com>
2024-01-14 17:05:37 +0530

    Update D_Flip_flop_posedge.v

--- Session_2/Codes/D_Flip_flop_posedge.v
+++ Session_2/Codes/D_Flip_flop_posedge.v
@@ -1,7 +1,7 @@
 module DFF_posedge(D,clk,rst,set,Q);
   input D,clk,rst,set;
   output reg Q;
-  always @(posedge clk or negedge rst or negedge set) begin
+  always @(posedge clk or negedge rst or negedge set) begin    // Try removing "negedge rst" and/or "negedge set" from the sensitivity list and observe circuit behaviour 
     if(rst==1'b0)
       Q<=1'b0;
     else if(set==1'b0)
